03fcb81cf40b86e554c4affd4c5ceadc1e517a31
Ale Brusco <alejandrobi89@gmail.com>
2015-06-27 14:15:16 -0300

    Otra clase

---
+++ Compilador/src/com/language/compilador/AuxiliaryElements.java
@@ -0,0 +1,10 @@
+package com.language.compilador;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AuxiliaryElements {
+	
+	public static List<String> functionList = new LinkedList<String>();
+	public static List<String> variableList = new LinkedList<String>();
+}
