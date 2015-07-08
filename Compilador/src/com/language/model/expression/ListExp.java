b55390a5c7e373ade351363b503129bc114b55c9
chulito <anvera@despegar.com>
2015-07-08 20:36:39 -0300

    ListExpression

--- Compilador/src/com/language/model/expression/ListExp.java
+++ Compilador/src/com/language/model/expression/ListExp.java
@@ -1,7 +1,9 @@
 package com.language.model.expression;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.language.types.ListType;
 import com.language.types.Types;
 
 public class ListExp extends Expression {
@@ -11,16 +13,18 @@ public class ListExp extends Expression {
 	public ListExp(List<Expression> list) {
 		this.list = list;
 	}
-	
-	@Override
-	public Types getType() {
-		return Types.list_type;
-	}
 
 	@Override
-	public Object eval() {
-		// TODO - Revisar
-		return null;
+	public Types eval() {
+		
+		List<Types> resultValues = new ArrayList<Types>();
+		
+		for (Expression e: this.list){
+			resultValues.add(e.eval());
+		}
+		
+		return new ListType(resultValues);
+		
 	}
 
 }
