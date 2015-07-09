4d775c0779fe5b34196ada7d2332c4daf1ec35e1
chulito <anvera@despegar.com>
2015-07-08 21:08:08 -0300

    joj

---
+++ Compilador/src/com/language/types/TypeType.java
@@ -0,0 +1,33 @@
+package com.language.types;
+
+import com.language.types.Types;
+
+public class TypeType extends Types {
+
+	String type;
+	
+	public TypeType(String type){
+		this.type = type;
+	}
+
+	@Override
+	public TypeEnum getType() {
+		return TypeEnum.type_type;
+	}
+
+	@Override
+	public void print() {
+		System.out.println(type);
+	}
+	
+	public String getTypeValue() {
+		return this.type;
+	}
+
+	@Override
+	public String toStringValue() {
+		return type;
+	}
+	
+
+}
