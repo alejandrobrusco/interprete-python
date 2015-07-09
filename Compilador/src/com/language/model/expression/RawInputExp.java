0affdba7ba7abddd4f512d892a54ab482f246528
Nacho <ibetancurt@despegar.com>
2015-07-08 21:01:57 -0300

    rawInput

--- Compilador/src/com/language/model/expression/RawInputExp.java
+++ Compilador/src/com/language/model/expression/RawInputExp.java
@@ -1,28 +1,34 @@
 package com.language.model.expression;
 
+import java.util.Scanner;
+
+import com.language.types.StringType;
 import com.language.types.Types;
 
 public class RawInputExp extends Expression {
 
-	
+	Expression expression;
 	
 	public RawInputExp(){
-		
+		this.expression = null;
 	}
 	
 	public RawInputExp(Expression expr){
-		
+		this.expression = expr;
 	}
 	
 	@Override
-	public Types getType() {
-		return Types.nondefined_type;
-	}
-
-	@Override
-	public Object eval() {
-		// TODO - Revisar
-		return null;
+	public Types eval() {
+		if (expression != null){
+			Types eval = expression.eval();
+			if (eval != null){
+				System.out.print(eval.toStringValue());
+			}
+		}
+		Scanner input = new Scanner(System.in);
+	    String lect = input.nextLine();
+	    input.close();
+		return new StringType(lect);
 	}
 
 }
