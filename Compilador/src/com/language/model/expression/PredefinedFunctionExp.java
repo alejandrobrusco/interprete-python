c5120a78feeb9798f52715c7db3c8001c52675e3
Nacho <ibetancurt@despegar.com>
2015-07-11 19:45:19 -0300

    final

--- Compilador/src/com/language/model/expression/PredefinedFunctionExp.java
+++ Compilador/src/com/language/model/expression/PredefinedFunctionExp.java
@@ -130,26 +130,139 @@ public class PredefinedFunctionExp extends Expression {
 					}
 				default:
 					//TODO EXCEPTION no es una funcion predefinida para dict
-					break;
+					return new VoidType();
 				}
 			}
 			else if (TypeEnum.string_type.equals(type)){
 				switch (predefinedId) {
 				case COUNT:
-					break;
+					if (parametersList.size()==1){
+						String str = ((StringType)variableType).getString();
+						Types subStrType = parametersList.get(0).eval();
+						if (TypeEnum.string_type.equals(subStrType.getType())){
+							String subStr = ((StringType)subStrType).getString();
+							int indexOf = 0;
+							int count = 0;
+							while (indexOf != -1){
+								indexOf = str.indexOf(subStr, 0);
+								if (indexOf != -1){
+									indexOf = indexOf + subStr.length();
+									count++;
+								}
+							}
+							return new IntegerType(count);
+						} else {
+							//TODO EXCEPTION no es un string
+							return new VoidType();
+						}
+					} else {
+						//TODO EXCEPTION no tiene un parametro
+						return new VoidType();
+					}
 				case FIND:
-					break;
+					if (parametersList.size()==1){
+						String str = ((StringType)variableType).getString();
+						Types subStrType = parametersList.get(0).eval();
+						if (TypeEnum.string_type.equals(subStrType.getType())){
+							String subStr = ((StringType)subStrType).getString();
+							int indexOf = str.indexOf(subStr);
+							return new IntegerType(indexOf);
+						} else {
+							//TODO EXCEPTION no es un string
+							return new VoidType();
+						}
+						
+					} else if (parametersList.size()==2){
+						String str = ((StringType)variableType).getString();
+						Types subStrType = parametersList.get(0).eval();
+						Types startType = parametersList.get(1).eval();
+						if (TypeEnum.string_type.equals(subStrType.getType()) && TypeEnum.int_type.equals(startType.getType()) ){
+							String subStr = ((StringType)subStrType).getString();
+							Integer i = ((IntegerType)startType).getInteger();
+							int indexOf = str.indexOf(subStr,i);
+							return new IntegerType(indexOf);
+						} else  if (TypeEnum.int_type.equals(startType.getType())) {
+							//TODO EXCEPTION no es un string
+							return new VoidType();
+						} else {
+							//TODO EXCEPTION no es un integer
+							return new VoidType();
+						} 
+					} else {
+						//TODO EXCEPTION no tiene un parametro
+						return new VoidType();
+					}
 				case JOIN:
-					break;
+					if (parametersList.size()==1){
+						String str = ((StringType)variableType).getString();
+						Types joinStrType = parametersList.get(0).eval();
+						if (TypeEnum.string_type.equals(joinStrType.getType())){
+							String joinStr = ((StringType)joinStrType).getString();
+							String retStr = String.join(str, joinStr);
+							return new StringType(retStr);
+						} else {
+							//TODO EXCEPTION no es un string
+							return new VoidType();
+						}
+					} else {
+						//TODO EXCEPTION no tiene un parametro
+						return new VoidType();
+					}
+					
 				case SPLIT:
-					break;
+					if (parametersList.size()==1){
+						String str = ((StringType)variableType).getString();
+						Types sepStrType = parametersList.get(0).eval();
+						if (TypeEnum.string_type.equals(sepStrType.getType())){
+							String sepStr = ((StringType)sepStrType).getString();
+							String[] strSplit = str.split(sepStr);
+							List<Types> strSplitList  = new ArrayList<Types>();
+							for (String s : strSplit) {
+								Types t = new StringType(s);
+								strSplitList .add(t);
+							}
+							return new ListType(strSplitList);
+						} else {
+							//TODO EXCEPTION no es un string
+							return new VoidType();
+						}
+					} else {
+						//TODO EXCEPTION no tiene un parametro
+						return new VoidType();
+					}
 				case REPLACE:
-					break;
+					if (parametersList.size()==2){
+						String str = ((StringType)variableType).getString();
+						Types oldStrType = parametersList.get(0).eval();
+						Types newStrType = parametersList.get(1).eval();
+						if (TypeEnum.string_type.equals(oldStrType.getType()) && TypeEnum.string_type.equals(newStrType.getType())){
+							String oldStr = ((StringType)oldStrType).getString();
+							String newStr = ((StringType)newStrType).getString();
+							String replaceFirst = str.replaceFirst(oldStr, newStr);
+							return new StringType(replaceFirst);
+						} else if (TypeEnum.string_type.equals(newStrType.getType())) {
+							//TODO EXCEPTION no es un string
+							return new VoidType();
+						} else {
+							//TODO EXCEPTION no es un string
+							return new VoidType();
+						}
+					} else {
+						//TODO EXCEPTION no tiene un parametro
+						return new VoidType();
+					}
 				case LENGTH:
-					break;
+					if (parametersList.size()==0){
+						String str = ((StringType)variableType).getString();
+						int length = str.length();
+						return new IntegerType(length);
+					} else {
+						//TODO EXCEPTION no tiene un parametro
+						return new VoidType();
+					}
 				default:
-					//TODO EXCEPTION no es una funcion predefinida para dict
-					break;
+					//TODO EXCEPTION no es una funcion predefinida para string
+					return new VoidType();
 				}
 			}
 			else if (TypeEnum.list_type.equals(type)){
@@ -208,16 +321,123 @@ public class PredefinedFunctionExp extends Expression {
 						return new VoidType();
 					}
 				case INDEX:
-					break;
+					if (parametersList.size()==1){
+						List<Types> list = ((ListType) variableType).getList();
+						Types value = parametersList.get(0).eval();
+						int count = 0;
+						int listSize = list.size();
+						while (count < listSize){
+							if (list.get(count).equals(value)){
+								return new IntegerType(count);
+							}
+							count++;
+						}
+						
+						//TODO EXCEPTION no se encuentra el indice
+						return new VoidType();
+						
+					} else if (parametersList.size()==2){
+						List<Types> list = ((ListType) variableType).getList();
+						Types value = parametersList.get(0).eval();
+						Types start = parametersList.get(1).eval();
+						
+						if (TypeEnum.int_type.equals(start.getType())){
+							Integer startValue = ((IntegerType)start).getInteger();
+							int count = startValue > list.size() ? list.size() : startValue;
+							int listSize = list.size();
+							while (count < listSize){
+								if (list.get(count).equals(value)){
+									return new IntegerType(count);
+								}
+								count++;
+							}
+							
+							//TODO EXCEPTION no se encuentra el indice
+							return new VoidType();
+							
+						} else {
+							//TODO EXCEPTION start no es tipo integer
+							return new VoidType();
+						}
+					} else if (parametersList.size()==3){
+						List<Types> list = ((ListType) variableType).getList();
+						Types value = parametersList.get(0).eval();
+						Types start = parametersList.get(1).eval();
+						Types end = parametersList.get(2).eval();
+						
+						if (TypeEnum.int_type.equals(start.getType()) && TypeEnum.int_type.equals(end.getType()) ){
+							Integer startValue = ((IntegerType)start).getInteger();
+							Integer endValue = ((IntegerType)end).getInteger();
+							int count = startValue > list.size() ? list.size() : startValue;
+							int tope = endValue > list.size() ? list.size() : endValue;
+							while (count < tope){
+								if (list.get(count).equals(value)){
+									return new IntegerType(count);
+								}
+								count++;
+							}
+							
+							//TODO EXCEPTION no se encuentra el indice
+							return new VoidType();
+							
+						} else if (TypeEnum.int_type.equals(end.getType())) {
+							//TODO EXCEPTION start no es tipo integer
+							return new VoidType();
+						} else {
+							//TODO EXCEPTION end  no es tipo integer
+							return new VoidType();
+						}
+					} else {
+						//TODO EXCEPTION cantidad de argumentos
+						return new VoidType();
+					}
 				case INSERT:
-					break;
+					if (parametersList.size()==2){
+						Types index = parametersList.get(0).eval();
+						Types value = parametersList.get(1).eval();
+						
+						if (TypeEnum.int_type.equals(index.getType())){
+							List<Types> list = ((ListType) variableType).getList();
+							Integer i = ((IntegerType)index).getInteger();
+							list.add(i, value);
+							return new VoidType();
+						} else {
+							//TODO EXCEPTION end  no es tipo integer
+							return new VoidType();
+						}						
+					} else {
+						//TODO EXCEPTION cantidad de argumentos
+						return new VoidType();
+					}
 				case POP:
-					break;
+					if (parametersList.size()==1){
+						Types index = parametersList.get(0).eval();
+					
+						if (TypeEnum.int_type.equals(index.getType())){
+							List<Types> list = ((ListType) variableType).getList();
+							Integer i = ((IntegerType)index).getInteger();
+							boolean ret = list.remove(i);
+							return new BooleanType(ret);
+						} else {
+							//TODO EXCEPTION end  no es tipo integer
+							return new VoidType();
+						}
+						
+					} else {
+						//TODO EXCEPTION cantidad de argumentos
+						return new VoidType();
+					}						
 				case SIZE:
-					break;
+					if (parametersList.size()==0){
+						List<Types> list = ((ListType) variableType).getList();
+						return new IntegerType(list.size());
+					} else {
+						//TODO EXCEPTION cantidad de argumentos
+						return new VoidType();
+					}						
 				default:
 					//TODO EXCEPTION no es una funcion predefinida para dict
-					break;
+					return new VoidType();
 				}
 			} else {
 				// TODO EXCEPTION type no valido
@@ -227,7 +447,6 @@ public class PredefinedFunctionExp extends Expression {
 			//TODO EXCEPTION no es un identificador
 			return new VoidType();
 		}
-		
 	}
 
 }
