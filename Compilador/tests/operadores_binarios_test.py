70e65c820dd6fbdc2daf33c5fb7a9f561730ddbb
Ale Brusco <alejandrobi89@gmail.com>
2015-07-12 01:32:19 -0300

    Tests de Operadores Binarios

---
+++ Compilador/tests/operadores_binarios_test.py
@@ -0,0 +1,897 @@
+# Inicializacion de variables
+s = "Hola"
+f = 1.5
+l = 2L
+i = 2
+b = True
+d = { 1 : "H", 2 : "o" }
+t = (1, 2, 3)
+lista = ["a", "b", "c"]
+
+print "ADD"
+print s + s
+print f + f
+print f + l
+print f + i
+print f + b
+print l + f
+print l + l
+print l + i
+print l + b
+print i + f
+print i + l
+print i + i
+print i + b
+print b + f
+print b + l
+print b + i
+print b + b
+print t + t
+print lista + lista
+
+print ""
+
+
+print "AND"
+
+print s and s
+print s and f
+print s and l
+print s and i
+print s and b
+print s and d
+print s and t
+print s and lista
+print f and s
+print f and f
+print f and l
+print f and i
+print f and b
+print f and d
+print f and t
+print f and lista
+print l and s
+print l and f
+print l and l
+print l and i
+print l and b
+print l and d
+print l and t
+print l and lista
+print i and s
+print i and f
+print i and l
+print i and i
+print i and b
+print i and d
+print i and t
+print i and lista
+print b and s
+print b and f
+print b and l
+print b and i
+print b and b
+print b and d
+print b and t
+print b and lista
+print d and s
+print d and f
+print d and l
+print d and i
+print d and b
+print d and d
+print d and t
+print d and lista
+print t and s
+print t and f
+print t and l
+print t and i
+print t and b
+print t and d
+print t and t
+print t and lista
+print lista and s
+print lista and f
+print lista and l
+print lista and i
+print lista and b
+print lista and d
+print lista and t
+print lista and lista
+
+print ""
+
+
+print "BAND"
+
+print l & l
+print l & i
+print l & b
+print i & l
+print i & i
+print i & b
+print b & l
+print b & i
+print b & b
+
+print ""
+
+
+print "BLSHIFT"
+
+print l << l
+print l << i
+print l << b
+print i << l
+print i << i
+print i << b
+print b << l
+print b << i
+print b << b
+
+print ""
+
+
+print "BOR"
+
+print l | l
+print l | i
+print l | b
+print i | l
+print i | i
+print i | b
+print b | l
+print b | i
+print b | b
+
+print ""
+
+
+print "BRSHIFT"
+
+print l >> l
+print l >> i
+print l >> b
+print i >> l
+print i >> i
+print i >> b
+print b >> l
+print b >> i
+print b >> b
+
+print ""
+
+
+print "BXOR"
+
+print l ^ l
+print l ^ i
+print l ^ b
+print i ^ l
+print i ^ i
+print i ^ b
+print b ^ l
+print b ^ i
+print b ^ b
+
+print ""
+
+
+print "DIVINT"
+
+print f // f
+print f // l
+print f // i
+print f // b
+print l // f
+print l // l
+print l // i
+print l // b
+print i // f
+print i // l
+print i // i
+print i // b
+print b // f
+print b // l
+print b // i
+print b // b
+
+print ""
+
+
+print "DIV"
+
+print f / f
+print f / l
+print f / i
+print f / b
+print l / f
+print l / l
+print l / i
+print l / b
+print i / f
+print i / l
+print i / i
+print i / b
+print b / f
+print b / l
+print b / i
+print b / b
+
+print ""
+
+
+print "EQUAL"
+
+print s == s
+print s == f
+print s == l
+print s == i
+print s == b
+print s == d
+print s == t
+print s == lista
+print f == s
+print f == f
+print f == l
+print f == i
+print f == b
+print f == d
+print f == t
+print f == lista
+print l == s
+print l == f
+print l == l
+print l == i
+print l == b
+print l == d
+print l == t
+print l == lista
+print i == s
+print i == f
+print i == l
+print i == i
+print i == b
+print i == d
+print i == t
+print i == lista
+print b == s
+print b == f
+print b == l
+print b == i
+print b == b
+print b == d
+print b == t
+print b == lista
+print d == s
+print d == f
+print d == l
+print d == i
+print d == b
+print d == d
+print d == t
+print d == lista
+print t == s
+print t == f
+print t == l
+print t == i
+print t == b
+print t == d
+print t == t
+print t == lista
+print lista == s
+print lista == f
+print lista == l
+print lista == i
+print lista == b
+print lista == d
+print lista == t
+print lista == lista
+
+print ""
+
+
+print "GREATEROREQUAL"
+
+print s >= s
+print s >= f
+print s >= l
+print s >= i
+print s >= b
+# No se implemento esta operacion con los diccionarios
+#print s >= d
+# No se implemento esta operacion con las tuplas
+#print s >= t
+# No se implemento esta operacion con las listas
+#print s >= lista
+print f >= s
+print f >= f
+print f >= l
+print f >= i
+print f >= b
+# No se implemento esta operacion con los diccionarios
+#print f >= d
+# No se implemento esta operacion con las tuplas
+#print f >= t
+# No se implemento esta operacion con las listas
+#print f >= lista
+print l >= s
+print l >= f
+print l >= l
+print l >= i
+print l >= b
+# No se implemento esta operacion con los diccionarios
+#print l >= d
+# No se implemento esta operacion con las tuplas
+#print l >= t
+# No se implemento esta operacion con las listas
+#print l >= lista
+print i >= s
+print i >= f
+print i >= l
+print i >= i
+print i >= b
+# No se implemento esta operacion con los diccionarios
+#print i >= d
+# No se implemento esta operacion con las tuplas
+#print i >= t
+# No se implemento esta operacion con las listas
+#print i >= lista
+print b >= s
+print b >= f
+print b >= l
+print b >= i
+print b >= b
+# No se implemento esta operacion con los diccionarios
+#print b >= d
+# No se implemento esta operacion con las tuplas
+#print b >= t
+# No se implemento esta operacion con las listas
+#print b >= lista
+# No se implemento esta operacion con los diccionarios
+#print d >= s
+#print d >= f
+#print d >= l
+#print d >= i
+#print d >= b
+#print d >= d
+#print d >= t
+#print d >= lista
+# No se implemento esta operacion con las tuplas
+#print t >= s
+#print t >= f
+#print t >= l
+#print t >= i
+#print t >= b
+# No se implemento esta operacion con los diccionarios
+#print t >= d
+#print t >= t
+#print t >= lista
+#print lista >= s
+#print lista >= f
+#print lista >= l
+#print lista >= i
+#print lista >= b
+# No se implemento esta operacion con los diccionarios
+#print lista >= d
+# No se implemento esta operacion con las tuplas
+#print lista >= t
+# No se implemento esta operacion con las listas
+#print lista >= lista
+
+print ""
+
+
+print "GREATER"
+
+print s > s
+print s > f
+print s > l
+print s > i
+print s > b
+# No se implemento esta operacion con los diccionarios
+#print s > d
+# No se implemento esta operacion con las tuplas
+#print s > t
+# No se implemento esta operacion con las listas
+#print s > lista
+print f > s
+print f > f
+print f > l
+print f > i
+print f > b
+# No se implemento esta operacion con los diccionarios
+#print f > d
+# No se implemento esta operacion con las tuplas
+#print f > t
+# No se implemento esta operacion con las listas
+#print f > lista
+print l > s
+print l > f
+print l > l
+print l > i
+print l > b
+# No se implemento esta operacion con los diccionarios
+#print l > d
+# No se implemento esta operacion con las tuplas
+#print l > t
+# No se implemento esta operacion con las listas
+#print l > lista
+print i > s
+print i > f
+print i > l
+print i > i
+print i > b
+# No se implemento esta operacion con los diccionarios
+#print i > d
+# No se implemento esta operacion con las tuplas
+#print i > t
+# No se implemento esta operacion con las listas
+#print i > lista
+print b > s
+print b > f
+print b > l
+print b > i
+print b > b
+# No se implemento esta operacion con los diccionarios
+#print b > d
+# No se implemento esta operacion con las tuplas
+#print b > t
+# No se implemento esta operacion con las listas
+#print b > lista
+# No se implemento esta operacion con los diccionarios
+#print d > s
+#print d > f
+#print d > l
+#print d > i
+#print d > b
+#print d > d
+#print d > t
+#print d > lista
+# No se implemento esta operacion con las tuplas
+#print t > s
+#print t > f
+#print t > l
+#print t > i
+#print t > b
+# No se implemento esta operacion con los diccionarios
+#print t > d
+# No se implemento esta operacion con las tuplas
+#print t > t
+#print t > lista
+# No se implemento esta operacion con las listas
+#print lista > s
+#print lista > f
+#print lista > l
+#print lista > i
+#print lista > b
+# No se implemento esta operacion con los diccionarios
+#print lista > d
+# No se implemento esta operacion con las tuplas
+#print lista > t
+# No se implemento esta operacion con las listas
+#print lista > lista
+
+print ""
+
+
+print "LESSOREQUAL"
+
+print s <= s
+print s <= f
+print s <= l
+print s <= i
+print s <= b
+# No se implemento esta operacion con los diccionarios
+#print s <= d
+# No se implemento esta operacion con las tuplas
+#print s <= t
+# No se implemento esta operacion con las listas
+#print s <= lista
+print f <= s
+print f <= f
+print f <= l
+print f <= i
+print f <= b
+# No se implemento esta operacion con los diccionarios
+#print f <= d
+# No se implemento esta operacion con las tuplas
+#print f <= t
+# No se implemento esta operacion con las listas
+#print f <= lista
+print l <= s
+print l <= f
+print l <= l
+print l <= i
+print l <= b
+# No se implemento esta operacion con los diccionarios
+#print l <= d
+# No se implemento esta operacion con las tuplas
+#print l <= t
+# No se implemento esta operacion con las listas
+#print l <= lista
+print i <= s
+print i <= f
+print i <= l
+print i <= i
+print i <= b
+# No se implemento esta operacion con los diccionarios
+#print i <= d
+# No se implemento esta operacion con las tuplas
+#print i <= t
+# No se implemento esta operacion con las listas
+#print i <= lista
+print b <= s
+print b <= f
+print b <= l
+print b <= i
+print b <= b
+# No se implemento esta operacion con los diccionarios
+#print b <= d
+# No se implemento esta operacion con las tuplas
+#print b <= t
+# No se implemento esta operacion con las listas
+#print b <= lista
+# No se implemento esta operacion con los diccionarios
+#print d <= s
+#print d <= f
+#print d <= l
+#print d <= i
+#print d <= b
+#print d <= d
+#print d <= t
+#print d <= lista
+# No se implemento esta operacion con las tuplas
+#print t <= s
+#print t <= f
+#print t <= l
+#print t <= i
+#print t <= b
+# No se implemento esta operacion con los diccionarios
+#print t <= d
+# No se implemento esta operacion con las tuplas
+#print t <= t
+#print t <= lista
+# No se implemento esta operacion con las listas
+#print lista <= s
+# No se implemento esta operacion con las listas
+#print lista <= f
+# No se implemento esta operacion con las listas
+#print lista <= l
+# No se implemento esta operacion con las listas
+#print lista <= i
+# No se implemento esta operacion con las listas
+#print lista <= b
+# No se implemento esta operacion con los diccionarios
+#print lista <= d
+# No se implemento esta operacion con las tuplas
+#print lista <= t
+# No se implemento esta operacion con las listas
+#print lista <= lista
+
+print ""
+
+
+print "LESS"
+
+print s < s
+print s < f
+print s < l
+print s < i
+print s < b
+# No se implemento esta operacion con los diccionarios
+#print s < d
+# No se implemento esta operacion con las tuplas
+#print s < t
+print s < lista
+print f < s
+print f < f
+print f < l
+print f < i
+print f < b
+# No se implemento esta operacion con los diccionarios
+#print f < d
+# No se implemento esta operacion con las tuplas
+#print f < t
+# No se implemento esta operacion con las listas
+#print f < lista
+print l < s
+print l < f
+print l < l
+print l < i
+print l < b
+# No se implemento esta operacion con los diccionarios
+#print l < d
+# No se implemento esta operacion con las tuplas
+#print l < t
+# No se implemento esta operacion con las listas
+#print l < lista
+print i < s
+print i < f
+print i < l
+print i < i
+print i < b
+# No se implemento esta operacion con los diccionarios
+#print i < d
+# No se implemento esta operacion con las tuplas
+#print i < t
+# No se implemento esta operacion con las listas
+#print i < lista
+print b < s
+print b < f
+print b < l
+print b < i
+print b < b
+# No se implemento esta operacion con los diccionarios
+#print b < d
+# No se implemento esta operacion con las tuplas
+#print b < t
+#print b < lista
+# No se implemento esta operacion con los diccionarios
+#print d < s
+#print d < f
+#print d < l
+#print d < i
+#print d < b
+#print d < d
+#print d < t
+#print d < lista
+# No se implemento esta operacion con las tuplas
+#print t < s
+#print t < f
+#print t < l
+#print t < i
+#print t < b
+# No se implemento esta operacion con los diccionarios
+#print t < d
+# No se implemento esta operacion con las tuplas
+#print t < t
+#print t < lista
+# No se implemento esta operacion con las listas
+#print lista < s
+#print lista < f
+#print lista < l
+#print lista < i
+#print lista < b
+# No se implemento esta operacion con los diccionarios
+#print lista < d
+# No se implemento esta operacion con las tuplas
+#print lista < t
+#print lista < lista
+
+print ""
+
+
+print "MOD"
+
+print f * f
+print f * l
+print f * i
+print f * b
+print l * f
+print l * l
+print l * i
+print l * b
+print i * f
+print i * l
+print i * i
+print i * b
+print b * f
+print b * l
+print b * i
+print b * b
+
+print ""
+
+
+print "MULT"
+
+print s * l
+print s * i
+print s * b
+print f * f
+print f * l
+print f * i
+print f * b
+print l * s
+print l * f
+print l * l
+print l * i
+print l * b
+print i * s
+print i * f
+print i * l
+print i * i
+print i * b
+print b * s
+print b * f
+print b * l
+print b * i
+print b * b
+
+print ""
+
+
+print "NOTEQUAL"
+
+print s != s
+print s != f
+print s != l
+print s != i
+print s != b
+print s != d
+print s != t
+print s != lista
+print f != s
+print f != f
+print f != l
+print f != i
+print f != b
+print f != d
+print f != t
+print f != lista
+print l != s
+print l != f
+print l != l
+print l != i
+print l != b
+print l != d
+print l != t
+print l != lista
+print i != s
+print i != f
+print i != l
+print i != i
+print i != b
+print i != d
+print i != t
+print i != lista
+print b != s
+print b != f
+print b != l
+print b != i
+print b != b
+print b != d
+print b != t
+print b != lista
+print d != s
+print d != f
+print d != l
+print d != i
+print d != b
+print d != d
+print d != t
+print d != lista
+print t != s
+print t != f
+print t != l
+print t != i
+print t != b
+print t != d
+print t != t
+print t != lista
+print lista != s
+print lista != f
+print lista != l
+print lista != i
+print lista != b
+print lista != d
+print lista != t
+print lista != lista
+
+print ""
+
+
+print "OR"
+
+print s or s
+print s or f
+print s or l
+print s or i
+print s or b
+print s or d
+print s or t
+print s or lista
+print f or s
+print f or f
+print f or l
+print f or i
+print f or b
+print f or d
+print f or t
+print f or lista
+print l or s
+print l or f
+print l or l
+print l or i
+print l or b
+print l or d
+print l or t
+print l or lista
+print i or s
+print i or f
+print i or l
+print i or i
+print i or b
+print i or d
+print i or t
+print i or lista
+print b or s
+print b or f
+print b or l
+print b or i
+print b or b
+print b or d
+print b or t
+print b or lista
+print d or s
+print d or f
+print d or l
+print d or i
+print d or b
+print d or d
+print d or t
+print d or lista
+print t or s
+print t or f
+print t or l
+print t or i
+print t or b
+print t or d
+print t or t
+print t or lista
+print lista or s
+print lista or f
+print lista or l
+print lista or i
+print lista or b
+print lista or d
+print lista or t
+print lista or lista
+
+print ""
+
+
+print "POW"
+
+print f ** f
+print f ** l
+print f ** i
+print f ** b
+print l ** f
+print l ** l
+print l ** i
+print l ** b
+print i ** f
+print i ** l
+print i ** i
+print i ** b
+print b ** f
+print b ** l
+print b ** i
+print b ** b
+
+print ""
+
+
+print "SUB"
+
+print f - f
+print f - l
+print f - i
+print f - b
+print l - f
+print l - l
+print l - i
+print l - b
+print i - f
+print i - l
+print i - i
+print i - b
+print b - f
+print b - l
+print b - i
+print b - b
+
+print ""
+
