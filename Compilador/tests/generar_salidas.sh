870859d5d177f386a9b173847921835a6a1e129c
Ale Brusco <alejandrobi89@gmail.com>
2015-07-17 00:37:12 -0300

    Emprolijamiento de Tests. Los que empiezan con "manual", requieren intervención del usuario y no se generan las salidas automáticamente con el generar_salidas.sh

--- Compilador/tests/generar_salidas.sh
+++ Compilador/tests/generar_salidas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 JAR_PATH="../build/jar/Compilador.jar"
-for i in $(ls *.py); do
-	python $i > ${i}_output.txt;
+for i in $(ls test*.py); do
+	python $i > salidas/${i}_output.txt;
 	java -jar ${JAR_PATH} ${i} > salidas/${i}_output_propio.txt;
 done
