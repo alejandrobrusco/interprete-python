c54d38fa248c8c230b3bc0119af163d3458e86cd
Ale Brusco <alejandrobi89@gmail.com>
2015-07-17 00:51:59 -0300

    Más mejores en el testing

--- Compilador/tests/generar_salidas.sh
+++ Compilador/tests/generar_salidas.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
+rm salidas/*
+echo "Se generaran las salidas de Python y del proyecto propio"
+echo ""
 JAR_PATH="../build/jar/Compilador.jar"
 for i in $(ls test*.py); do
 	python $i > salidas/${i}_output.txt;
 	java -jar ${JAR_PATH} ${i} > salidas/${i}_output_propio.txt;
 done
+
+clear
+echo "Pueden haber diferencias entre las salidas de Python y el proyecto propio porque:"
+echo "- Los elementos de un diccionario no tienen un orden"
+echo "- Errores de redondeo en divisiones y potencias"
+echo ""
+echo "PRESIONE UNA TECLA PARA REALIZAR EL DIFF ENTRE LAS SALIDAS"
+read
+for i in $(ls test*.py); do
+	clear	
+	echo "------------------- DIFF de ${i} -------------------"
+	diff salidas/${i}_output*
+	echo ""
+	echo "--------------------------------------------------------------------"
+	echo "PRESIONA UNA TECLA PARA IR AL SIGUIENTE TEST"
+	read
+done
+clear
+echo "FIN !"
